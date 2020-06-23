f296c0f89139ae7bac6154e494e1ea56293b2ebc
yotamba12 <yotambaram@gmail.com>
2020-06-23 13:07:52 -0700

    api url update

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://gotutor.herokuapp.com"
+const BASE_URL = "https://gotutorback.herokuapp.com"
 //const BASE_URL = "http://localhost:8080"
 
 const API = {
